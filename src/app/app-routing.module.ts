e1a6520bdf0698ae44b20e09dbae1c85934a6211
ericbarima <eric.barima.external@atos.net>
2020-10-06 16:09:19 +0100

    Okay with menu creation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,37 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SiteMapComponent } from './site-map/site-map.component';
+import { CatalogueSearchComponent } from './catalogue-search/catalogue-search.component';
+import { CatalogueMaintenanceComponent } from './catalogue-maintenance/catalogue-maintenance.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: 'home', pathMatch: 'full' 
+  },
+  {
+    path: "home",
+    component: HomeComponent
+  },
+  {
+    path: "site-map",
+    component: SiteMapComponent
+  },
+  {
+    path: "catalogue-search",
+    component: CatalogueSearchComponent
+  },
+  {
+    path: "maintanance-catalogue",
+    component: CatalogueMaintenanceComponent
+  },
+  {
+    path: "about",
+    component: AboutComponent
+  } 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SiteMapComponent } from './site-map/site-map.component';
+import { MaintenanceComponent } from './maintenance/maintenance.component';
+import { CatalogueSearchComponent } from './catalogue-search/catalogue-search.component';
+import { CatalogueMaintenanceComponent } from './catalogue-maintenance/catalogue-maintenance.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SiteMapComponent,
+    MaintenanceComponent,
+    CatalogueSearchComponent,
+    CatalogueMaintenanceComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
